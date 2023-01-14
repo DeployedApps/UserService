0be282652822d73cbeb6ef43756073cd3e6974c8
- <p28@LAMU0Q7GF7X05W6.uhc.com>
2023-01-14 20:06:02 +0530

    Docs API Server

--- src/main/java/com/jat/UserService/UserServiceApplication.java
+++ src/main/java/com/jat/UserService/UserServiceApplication.java
@@ -1,5 +1,9 @@
 package com.jat.UserService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
